5641911ce3f26e78f225360443333240d82c2fa0
aaboud@ejada.com <aaboud@ejada.com>
2025-07-31 13:20:16 +0300

    Fix: correct S3 bucket name in deploy script

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,5 +1,7 @@
 # aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
 
 # Replace the [udagram-test01] below with the S3 bucket name
-aws s3 cp --recursive --acl public-read ./build s3://myawsbucket-76830077498/
+# aws s3 cp --recursive --acl public-read ./build s3://myawsbucket-76830077498/
+aws s3 cp --recursive --acl public-read ./build s3://myawsbucket-768300774983/
+
 # The trailing / at the end of the URL is important. 
